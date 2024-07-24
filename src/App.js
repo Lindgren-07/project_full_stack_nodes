a4061c72907b14b3319c25b73305077f9464cd86
João Lindgren <joaolindgren5@gmail.com>
2024-07-24 19:29:21 -0300

    primeiro commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import './app.css';
+import './global.css';
+import './sidebar.css';
+import './main.css';
+import Notes  from './Componentes/Notes';
+
+
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="app">
+      <aside>
+        <strong>Caderno de notas</strong>
+
+        <form>
+
+        <div className="input-block">
+          <label htmlFor="title">Titulo da Anotação</label>
+          <input></input>
+        </div>
+
+        <div className="input-block">
+          <label htmlFor="nota">Anotações</label>
+          <textarea></textarea>
+        </div>
+
+        <button type="submit">Salvar</button>
+        </form>
+      </aside>
+      <main>
+
+        <ul>
+          <Notes></Notes>
+        </ul>
+
+      </main>
     </div>
   );
 }
---
+++ src/Componentes/Notes/index.js
@@ -0,0 +1,70 @@
+import React from "react";
+
+function Notes() {
+    return(
+        <>
+         <li class="notepad-infos">
+            <div>
+              <strong>Fazer Compras</strong>
+              <div>
+                x
+              </div>
+            </div>
+
+            <textarea>lorem</textarea>
+            <span>!</span>
+          </li>
+
+          <li class="notepad-infos">
+            <div>
+              <strong>Fazer Compras</strong>
+              <div>
+                x
+              </div>
+            </div>
+
+            <textarea>lorem</textarea>
+            <span>!</span>
+          </li>
+
+          <li class="notepad-infos">
+            <div>
+              <strong>Fazer Compras</strong>
+              <div>
+                x
+              </div>
+            </div>
+
+            <textarea>lorem</textarea>
+            <span>!</span>
+          </li>
+
+          <li class="notepad-infos">
+            <div>
+              <strong>Fazer Compras</strong>
+              <div>
+                x
+              </div>
+            </div>
+
+            <textarea>lorem</textarea>
+            <span>!</span>
+          </li>
+
+          <li class="notepad-infos">
+            <div>
+              <strong>Fazer Compras</strong>
+              <div>
+                x
+              </div>
+            </div>
+
+            <textarea>lorem</textarea>
+            <span>!</span>
+          </li>
+
+        </>
+    )
+}
+
+export default Notes;
